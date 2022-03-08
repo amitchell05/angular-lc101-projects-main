c9ff688675f6f8d30e06f21d55f0f18a8aee68c5
Angela Mitchell <anglarenmit@gmail.com>
2022-03-07 18:25:46 -0600

    move input-practice solution to new repo

--- lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
+++ lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-   selector: 'movie-list',
-   templateUrl: './movie-list.component.html',
-   styleUrls: ['./movie-list.component.css']
+  selector: "movie-list",
+  templateUrl: "./movie-list.component.html",
+  styleUrls: ["./movie-list.component.css"],
 })
 export class MovieListComponent implements OnInit {
-   movies = ['Toy Story', 'The Shining', 'Sleepless in Seattle', 'The Martian'];
+  movies: string[] = [
+    "Toy Story",
+    "The Shining",
+    "Sleepless in Seattle",
+    "The Martian",
+  ];
+  errorMessage: string = "";
 
-   constructor() { }
+  constructor() {}
 
-   ngOnInit() {
-   }
+  ngOnInit() {}
+
+  addMovie(newTitle: string) {
+    if (this.movies.includes(newTitle)) {
+      this.errorMessage = `${newTitle} is already in your movie list.`;
+    } else if (newTitle === "") {
+      this.errorMessage = "Please enter a movie title.";
+    } else {
+      this.movies.push(newTitle);
+    }
+  }
 }
