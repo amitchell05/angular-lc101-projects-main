36bf38b811df67a38f988b230c83161b161dce19
Angela Mitchell <anglarenmit@gmail.com>
2022-03-07 18:48:15 -0600

    move ngif-practice code into new repo

--- lesson2/examples/ngif-practice/src/app/chores/chores.component.ts
+++ lesson2/examples/ngif-practice/src/app/chores/chores.component.ts
@@ -1,19 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-   selector: 'chores',
-   templateUrl: './chores.component.html',
-   styleUrls: ['./chores.component.css']
+  selector: "chores",
+  templateUrl: "./chores.component.html",
+  styleUrls: ["./chores.component.css"],
 })
 export class ChoresComponent implements OnInit {
-   chores = ['Empty dishwasher', 'Complete LaunchCode prep work', 'Buy groceries'];
-   finishedChores = [];
+  chores = [];
+  finishedChores = [
+    "Complete LaunchCode prep work",
+    "Buy groceries",
+    "Clean kitchen",
+    "Empty dishwasher",
+  ];
 
-   targetImage = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
+  targetImage =
+    "https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png";
 
-   constructor() { }
+  constructor() {}
 
-   ngOnInit() {
-   }
+  isWorkingHard() {
+    return this.chores.length < this.finishedChores.length;
+  }
 
+  isAllowanceAvailable() {
+    return (
+      this.chores.length === 0 ||
+      this.finishedChores.length - this.chores.length >= 3
+    );
+  }
+
+  areChoresComplete() {
+    return this.chores.length === 0 && this.finishedChores.length >= 4;
+  }
+
+  ngOnInit(): void {}
 }
